75b625b9bea1dab65625c0c272125735a08f6c4e
Oliver0804 <icetzsr@gmail.com>
2024-01-24 21:13:58 +0800

    add pico json

--- i2cExporer/i2cExporer.ino
+++ i2cExporer/i2cExporer.ino
@@ -1,3 +1,6 @@
+/*
+ * https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+ */
 #include <Wire.h>
 #include <U8g2lib.h>
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
